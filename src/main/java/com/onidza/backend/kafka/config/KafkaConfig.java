ae561d2fb8f8abccd654c883293ec174e1687439
Ivan Stupnitsky <onizukatiqer@gmail.com>
2026-02-03 20:02:43 +0300

    add kafka in compose.yaml, successfully

---
+++ src/main/java/com/onidza/backend/kafka/config/KafkaConfig.java
@@ -0,0 +1,10 @@
+package com.onidza.backend.kafka.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.ProducerFactory;
+
+@Configuration
+public class KafkaConfig {
+
+//    public ProducerFactory<String, String>producerFactory =
+}
--- src/main/java/com/onidza/backend/repository/ClientRepository.java
+++ src/main/java/com/onidza/backend/repository/ClientRepository.java
@@ -1,8 +1,6 @@
 package com.onidza.backend.repository;
 
 import com.onidza.backend.model.entity.Client;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +8,4 @@ import org.springframework.stereotype.Repository;
 public interface ClientRepository extends JpaRepository<Client, Long> {
 
     boolean existsByEmail(String email);
-
-    Page<Client> findAllBy(Pageable pageable);
 }
